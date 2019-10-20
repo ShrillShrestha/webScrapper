6bdc35b5fd0e05216119906414701a01cda3b586
Shrill Shrestha <shrill.shrestha@gmail.com>
2019-10-20 12:02:15 -0500

    Refactor: add connection checking to avoid crash if connection fails
    Add User Agent to have a normal web browsing

--- Java/webScrap/src/Crawler.java
+++ Java/webScrap/src/Crawler.java
@@ -2,7 +2,6 @@
 import essential libraries
  */
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import org.jsoup.Connection;
@@ -16,33 +15,38 @@ define crawler class
 use the Jsoup library to crawl around the web
  */
 public class Crawler {
-
+    /*
+    Use desired User agent to make the connection to act it like a normal web browsing
+     */
+    private String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36";
     private List<String> links = new LinkedList<>(); //list of Urls
     private Document htmlDocument; //the document page of our web page
-    /*
-    no argument constructor
-     */
-    public Crawler(){
-
-    }
 
     /**
      * collects the links in that website
      * @return list of all the links in the website
      */
     public List<String> getLinks(){
-        return new ArrayList<String>();
+        return this.links;
     }
 
     /**
      * the craweler method that crawls the website
      * @param url the url which is being used for the time being
      */
-    public void crawl(String url){
+    public boolean crawl(String url){
         try{
-            Connection connection = Jsoup.connect(url);
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
+
+            if(connection.response().statusCode() == 200){
+                System.out.println("Visiting the " + url + ".....");
+            }
+            if(!connection.response().contentType().contains("text/html")){
+                System.out.println("Received other than html page!");
+                return false;
+            }
 
             System.out.println("Received web page at: " + url);
 
@@ -51,9 +55,11 @@ public class Crawler {
             for(Element link: linksOnPage){
                 this.links.add(link.absUrl("href"));
             }
+            return true;
 
         }catch (IOException ioe){
             System.out.println("Error in HTTPS request " + ioe);
+            return false;
         }
     }
 
@@ -63,6 +69,10 @@ public class Crawler {
      * @return boolean value to represent if the word was found in the website
      */
     public boolean searchForWord(String word){
+        if(this.htmlDocument == null){
+            System.out.println("Error! Call crawl() before analyzing the document");
+            return false;
+        }
         System.out.println("Searching for " + word + ".....");
         String bodyText = this.htmlDocument.body().text(); //document body text stored in a string variable
         return bodyText.toLowerCase().contains(word.toLowerCase()); //string method contains to search for the sequence of the char in word in the body test
