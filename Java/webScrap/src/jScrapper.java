7fedd72c7e136cb965fea897725576593fc94837
Shrill Shrestha <shrill.shrestha@gmail.com>
2019-10-19 21:58:11 -0500

    Feat: add nextUrl method to iterate through Url to crawl

--- Java/webScrap/src/jScrapper.java
+++ Java/webScrap/src/jScrapper.java
@@ -1,2 +1,19 @@
-public class jScrapper {
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.List;
+
+public class jScrapper{
+    private static final int MAX_PAGES_TO_SEARCH = 5;
+    private Set<String> pageVisited = new HashSet<>();
+    private List<String> pagesToVisit = new LinkedList<>();
+
+    private String nextUrl(){
+        String nextURL = pagesToVisit.remove(0);
+        while(pageVisited.contains(nextURL)){
+            nextURL = pagesToVisit.remove(0);
+        }
+        pageVisited.add(nextURL);
+        return nextURL;
+    }
 }
